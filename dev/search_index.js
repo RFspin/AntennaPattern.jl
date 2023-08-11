c02738e1a9383849c60f364ed06b4ab50df569d3
Documenter.jl <documenter@juliadocs.github.io>
2023-08-11 22:27:08 +0000

    build based on c1a7fdc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = APattern","category":"page"},{"location":"#APattern","page":"Home","title":"APattern","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for APattern.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [APattern]","category":"page"}]
+}
